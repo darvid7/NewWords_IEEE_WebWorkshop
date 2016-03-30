70872d53dfb6a5b075dc19779873383e1d44e215
darvid7 <bvdarvid@gmail.com>
2016-03-30 14:52:13 +1100

    3rd Commit, database working

--- Client/index.js
+++ Client/index.js
@@ -5,6 +5,38 @@ document.write("hello..again! .. I am from Index.js in Client");
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MyController', function($scope){
-    $scope.newWord = 'cat';
+// interface with internet so uses HTTP
+// interface with server, retrieve our words
+// handles interaction with server
+// while controller handles html part
+myApp.service('HistoryService', function($http){
+    var baseUrl = "http://localhost:8080/";          // string, address of server
+    // function saveWord inside myApp.service
+    this.saveWord = function(newWord){
+        var url = baseUrl + "saveCurrent";           // server + save current (address of our post handler)
+        return $http.post(url, {"word":newWord})    // send data from website to server
+    }
+    // HistoryService.saveWord(newWord)
 });
+
+
+
+
+// note we have $scope as it uses the HTML file
+myApp.controller('MyController', function($scope, HistoryService){
+    $scope.newWord = 'cat';
+    
+    $scope.saveThisWord = function () {
+        HistoryService.saveWord($scope.newWord)
+            .then(saveSuccess, error)
+    };
+
+    function saveSuccess(json){
+        console.log(json)
+    }
+
+    function error(err){
+        console.log(err)
+    }
+});
+
--- Server/server.js
+++ Server/server.js
@@ -3,11 +3,43 @@
  */
 
 var express = require('express');           // go into node modules and find the express folder
+var bodyParser = require('body-parser');
+var Nedb = require('nedb');
+
+
 var app = express();                        // remember the ()
 
+
+var database = new Nedb ({filename: './data/data.db', autoload: true});
+// . means this folder then data then data.db
+
 app.use(express.static('../client'));       // sit and waits for requests
-                                            
+
+app.use(bodyParser.json());                 // everything gets passed through .use
+
+// accept info from client via this post, if someone posts to this section of the site
+// localhost:8080/saveCurrent will go to here
+app.post('/saveCurrent', function(req, res){        // req = info sent from webapp, has ip, time, date, data etc access via dot
+    // req.data
+    // response, wha twe will send back
+    //res.send();
+    var data = {
+            word: req.body.word,
+            data: Date.now()
+        };
+
+    var done = function() {
+        console.log("I just wrote to the database");
+        res.end("done")
+    };
+    database.insert(data, done)
+});
+
+
+
 
 var port = 8080;
 app.listen(port);
 console.log("listening on port: " + port);
+
+// note: any db is like mongo db but just stores your fils in a txt file
